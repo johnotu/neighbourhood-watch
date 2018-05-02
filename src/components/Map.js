6fd2f46f1fb4848e96395de7daf59ed9e47cd931
John Otu <johnne01@gmail.com>
2018-05-02 13:40:30 +0000

    displayed multiple location markersaround location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
+const incidents = [
+  { id: 0, latAdd: 0.010029, lngAdd: 0.010031 },
+  { id: 1, latAdd: 0.000023, lngAdd: 0.000054 },
+  { id: 2, latAdd: 0.020098, lngAdd: 0.000064 },
+  { id: 3, latAdd: 0.023163, lngAdd: 0.010055 },
+];
+
 class Map extends Component {
 
   constructor(props) {
@@ -33,7 +40,14 @@ class Map extends Component {
         defaultCenter={{ lat: this.state.currentLocation.lat, lng: this.state.currentLocation.lng }}
         defaultZoom={13}
       > 
-        <Marker position={{lat: this.state.currentLocation.lat, lng:  this.state.currentLocation.lng}} />  
+        {console.log(incidents, this.state.currentLocation)}
+        {incidents.map(incident => (
+          <Marker
+            key={incident.id}
+            position={{ lat: incident.latAdd + this.state.currentLocation.lat, lng: incident.lngAdd + this.state.currentLocation.lng }} />
+        ))}
+        {/* <Marker key={0} position={{ lat: this.state.currentLocation.lat, lng: this.state.currentLocation.lng }} /> 
+        <Marker key={1} position={{ lat: this.state.currentLocation.lat + 0.020611, lng: this.state.currentLocation.lng + 0.020711 }} /> */}
       </GoogleMap>
     ));
 
